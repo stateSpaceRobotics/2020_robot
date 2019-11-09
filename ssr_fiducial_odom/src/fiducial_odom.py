8a0e3c32f7526394f44ff0683ea4afdb91590028
Rhett Fereday <gmf78@msstate.edu>
2019-11-09 15:58:06 -0600

    Initial Fiducial Odometry
    This package handles the receiving of transformation information about ARuco markers form the aruco_detect package and the outputs odometry information about the markers.

---
+++ ssr_fiducial_odom/src/fiducial_odom.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import PoseWithCovariance, Point, Quaternion, Pose
+from nav_msgs.msg import Odometry
+from fiducial_msgs.msg import FiducialTransformArray
+
+def fiducialTransformCallback(fid_transforms):
+
+    for transform in fid_transforms.transforms:
+
+        odom_msg = Odometry()
+        odom_msg.header.stamp = fid_transforms.header.stamp
+        odom_msg.header.frame_id = fid_transforms.header.frame_id
+        odom_msg.child_frame_id = "fiducial_" + str(transform.fiducial_id)
+        odom_msg.pose.pose = Pose(Point(transform.transform.translation.x, transform.transform.translation.y, transform.transform.translation.z), transform.transform.rotation)
+        odomPublisher.publish(odom_msg)
+
+odomPublisher = 0
+
+def main():
+    global odomPublisher
+
+    rospy.init_node('listener', anonymous = True)
+    rospy.Subscriber("/fiducial_transforms", FiducialTransformArray, fiducialTransformCallback)
+    odomPublisher = rospy.Publisher('/fiducials/odom', Odometry, queue_size=50)
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
