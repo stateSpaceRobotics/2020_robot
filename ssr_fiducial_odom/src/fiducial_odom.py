a421f2ea5d2780beb24146c3cfc26dac927d1ebe
Zack Taylor <zdt1997@gmail.com>
2020-01-24 17:11:31 -0600

    Hopefully done

--- ssr_fiducial_odom/src/fiducial_odom.py
+++ ssr_fiducial_odom/src/fiducial_odom.py
@@ -1,27 +1,21 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import PoseWithCovariance, Point, Quaternion, Pose, PoseWithCovarianceStamped
+import tf
+import geometry_msgs.msg 
 from nav_msgs.msg import Odometry
 from fiducial_msgs.msg import FiducialTransformArray
 
 def fiducialTransformCallback(fid_transform):
+    br = tf.TransformBroadcaster()
+    br.sendTransform(fid_transform[0], fid_transform[1], rospy.Time.now(), "fiducialTransforms/fiducial_" + str(fid_transform.fiducial_id), "camera_link")
 
-    odom_msg = Odometry()
-    odom_msg.header = fid_transform.header
-    odom_msg.child_frame_id = "camera_link"
-    odom_msg.pose = fid_transform.pose
-    odomPublisher.publish(odom_msg)
-
-odomPublisher = 0
 
 def main():
-    global odomPublisher
 
     rospy.init_node('listener', anonymous = True)
     rospy.Subscriber("/fiducial_pose", PoseWithCovarianceStamped, fiducialTransformCallback)
-    odomPublisher = rospy.Publisher('/fiducials/odom', Odometry, queue_size=50)
-
+    
     rospy.spin()
 
 if __name__ == '__main__':
